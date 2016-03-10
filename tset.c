30a05de152dbecb89113c40656b0942f423fc8e8
Scott Grodberg <scott.grodberg@rutgers.edu>
2016-03-10 12:20:53 -0500

    Added a check around fopen() in case the specified testcases file doesnt exist

--- tset.c
+++ tset.c
@@ -37,69 +37,73 @@ int main (int argc, char **argv) {
 	FILE *fp;
 	fp = fopen(argv[2], "r");
 
-	int i;
-	i=0;
-	while(fgets(line, MAXLINELEN, fp)){
+	if (!fp) {
+		printf("Could not open the testcases file: %s\n", argv[2]);
+		exit(1);
+	} else {
 
-		if (*line && i) {
-			/* get a line, up to MAXLINELEN chars from fp.  done if NULL */
-			sscanf (line, "%[^|]|%[^|]|%[^\n]", input, expected, actual);
+		int i;
+		i=0;
+		while(fgets(line, MAXLINELEN, fp)){
+
+			if (*line && i) {
+				/* get a line, up to MAXLINELEN chars from fp.  done if NULL */
+				sscanf (line, "%[^|]|%[^|]|%[^\n]", input, expected, actual);
+				
+
+				if (debug){
+					/* convert the string to a long int */
+					printf ("%s | %s | %s\n", input, expected, actual);
+				}
+
 				/* we have the program args and what is expected */
 				/* build the command for popen() */
 				snprintf(command, sizeof(command), "%s %s 2>&1", argv[1], input);
 			
+				if (debug){
+					/* show the built-up command string and test color output */ 
+					printf ("Here is the command: %s\n", command);
+				}
 
-			if (debug){
-				/* convert the string to a long int */
-				printf ("%s | %s | %s\n", input, expected, actual);
-			}
+				FILE *pp;
+				pp = popen(command,"r");
+				char output[100];
 
-			/* we have the program args and what is expected */
-			/* build the command for popen() */
-			snprintf(command, sizeof(command), "./%s %s 2>&1", argv[1], input);
+				while (fgets(output, MAXLINELEN, pp)) {
+
+					char *result;
+					
+					/* this assumes there is a newline that needs to get removed, but we will add a null char */
+					int len_output = strlen(output);
+					if (debug) printf("here is the line length of the result from the tested program %d\n",len_output);
+
+
+					result = (char*)malloc(sizeof(char) * len_output); 
+					
+					sscanf(output, "%[^\n]", result);
+					result[len_output] = '\0';
+
+					if (debug) printf("Here is the command with newline removed: %s\n", result);
+				
+					printf ("Input: %s  Expected: %s  Actual: %s ", input, expected, result);
+					if (strcmp(result,expected) ==0 )
+						printf(ANSI_OUT_PASS "PASS" ANSI_OUT_RESET "\n");
+					else
+						printf (ANSI_OUT_FAIL "FAIL" ANSI_OUT_RESET "\n");
 		
-			if (debug){
-				/* show the built-up command string and test color output */ 
-				printf ("Here is the command: %s\n", command);
-			}
+				}
 
-			FILE *pp;
-			pp = popen(command,"r");
-			char output[100];
-
-			while (fgets(output, MAXLINELEN, pp)) {
-
-				char *result;
-				
-				/* this assumes there is a newline that needs to get removed, but we will add a null char */
-				int len_output = strlen(output);
-				if (debug) printf("here is the line length of the result from the tested program %d\n",len_output);
-
-
-				result = (char*)malloc(sizeof(char) * len_output); 
-				
-				sscanf(output, "%[^\n]", result);
-				result[len_output] = '\0';
-
-				if (debug) printf("Here is the command with newline removed: %s\n", result);
+				pclose(pp);
 			
-				printf ("Input: %s  Expected: %s  Actual: %s ", input, expected, result);
-				if (strcmp(result,expected) ==0 )
-					printf(ANSI_OUT_PASS "PASS" ANSI_OUT_RESET "\n");
-				else
-					printf (ANSI_OUT_FAIL "FAIL" ANSI_OUT_RESET "\n");
-	
+
+
+			
 			}
-
-			pclose(pp);
-		
-
-
-		
+			i++;
 		}
-		i++;
-	}
-	fclose(fp);  
+		fclose(fp);  
+
+	} // end while
 
 	/* reset the output ansi color and done */
 	printf("Done!\n");
